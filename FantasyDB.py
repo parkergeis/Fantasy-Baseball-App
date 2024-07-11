609fae870b735d094803e3b949a7efd191c78236
parkergeis <parkert.geis@icloud.com>
2024-07-10 23:21:44 -0400

    Update FantasyDB.py

--- FantasyDB.py
+++ FantasyDB.py
@@ -7,7 +7,7 @@ url = "https://docs.google.com/spreadsheets/d/1DO97mLDDqkgq-QoQCuXgJfRefyocmHSI1
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
+data = conn.read(spreadsheet=url)
 st.dataframe(data)
 
 
