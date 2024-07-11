65ec81d0ffbc7a2050ece90b9ec8bd0014311fc5
parkergeis <parkert.geis@icloud.com>
2024-07-10 23:26:04 -0400

    Update FantasyDB.py

--- FantasyDB.py
+++ FantasyDB.py
@@ -1,13 +1,11 @@
-# Fantasy DB
+# example/st_app.py
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1DO97mLDDqkgq-QoQCuXgJfRefyocmHSI1-OJxBafR6U/edit?gid=0#gid=0"
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(data)
-
-
