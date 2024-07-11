b461621e702b17062861584570d789d5ab670d25
parkergeis <parkert.geis@icloud.com>
2024-07-10 23:15:03 -0400

    Create FantasyDB.py

---
+++ FantasyDB.py
@@ -0,0 +1,13 @@
+# Fantasy DB
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1DO97mLDDqkgq-QoQCuXgJfRefyocmHSI1-OJxBafR6U/edit?gid=0#gid=0"
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
+
+
