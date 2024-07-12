5b908132e3624e873a4ad3c17447db8de3a59f5b
parkergeis <parkert.geis@icloud.com>
2024-07-12 12:52:08 -0400

    Update FantasyDB.py

--- FantasyDB.py
+++ FantasyDB.py
@@ -22,6 +22,7 @@ def highlight_hr_val(data):
     color = 'green' if data['HR'] == 'WIN' else 'red' if data['HR'] == 'LOSS' else ''
     return ['background-color: %s' % color if column == 'HR_val' else '' for column in data.index]
 
+st.dataframe(CurrentWeeklyData.style.hide(axis="index"))
 st.dataframe(CurrentWeeklyData.style.apply(highlight_hr_val, axis=1))
 
 
