b27dab5a140a2a3d526e44bad2e10a64f8ccede3
parkergeis <parkert.geis@icloud.com>
2024-07-12 09:13:55 -0400

    Updating

--- FantasyDB.py
+++ FantasyDB.py
@@ -2,8 +2,20 @@
 
 import streamlit as st
 import pandas as pd
+import altair as alt
+import plotly.express as px
 
-data = pd.read_excel('data/FantasyData.xlsx')
-st.dataframe(data)
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+WeeklyData = pd.read_excel('data/FantasyData.xlsx', sheet_name='WeeklyData')
+Standings = pd.read_excel('data/FantasyData.xlsx', sheet_name='PreviousStandings')
+Rosters = pd.read_excel('data/FantasyData.xlsx', sheet_name='Rosters')
+
 
 
