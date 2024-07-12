57e9168f6fad6b733d84c604c7e3f92f5bc9eec3
parkergeis <parkert.geis@icloud.com>
2024-07-12 13:00:17 -0400

    Update FantasyDB.py

--- FantasyDB.py
+++ FantasyDB.py
@@ -17,7 +17,7 @@ Standings = pd.read_excel('data/FantasyData.xlsx', sheet_name='PreviousStandings
 Rosters = pd.read_excel('data/FantasyData.xlsx', sheet_name='Rosters')
 max_week = WeeklyData['Week'].max()
 CurrentWeeklyData = WeeklyData[WeeklyData['Week'] == max_week]
-
+CurrentWeeklyData = CurrentWeeklyData.reset_index(drop=True)
 def highlight_hr_val(data):
     color = 'green' if data['HR'] == 'WIN' else 'red' if data['HR'] == 'LOSS' else ''
     return ['background-color: %s' % color if column == 'HR_val' else '' for column in data.index]
