9da2eda63c136d5b9e5d9de48505fe547aef0426
parkergeis <parkert.geis@icloud.com>
2024-07-12 12:43:11 -0400

    Update FantasyDB.py

--- FantasyDB.py
+++ FantasyDB.py
@@ -18,9 +18,18 @@ Rosters = pd.read_excel('data/FantasyData.xlsx', sheet_name='Rosters')
 max_week = WeeklyData['Week'].max()
 CurrentWeeklyData = WeeklyData[WeeklyData['Week'] == max_week]
 
+def color_win(data):
+    color = 'green' if data['HR'] == 'WIN' else 'red' if data['HR'] == 'LOSS' else 'black'
+    return f'<td style="color: {color}">{data["HR_val"]}</td>'
 
-st.markdown('#### Current Week')
-st.dataframe(CurrentWeeklyData)
+df_html = (CurrentWeeklyData.style
+           .apply(lambda x: CurrentWeeklyData.apply(color_win, axis=1), subset=['HR_val'])
+           .format({'HR_val': '{}'}, escape=False)
+           .render())
+
+st.markdown(df_html, unsafe_allow_html=True)
+
+st.markdown(df_html, unsafe_allow_html=True)
 
 
 
