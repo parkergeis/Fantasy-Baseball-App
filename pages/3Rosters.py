b29aa1a15053e81e3d8ba9ac4d42d0e6296445bd
parkergeis <parkert.geis@icloud.com>
2024-08-05 17:31:46 -0400

    test

--- pages/3Rosters.py
+++ pages/3Rosters.py
@@ -32,7 +32,7 @@ with st.sidebar:
     st.button('Reset Filters', on_click=reset_filters)
     
     team_list = list(Rosters.Owner.unique())
-    team_list = team_list[team_list.isna() == False]
+    team_list = [team for team in team_list if team is not None]
     team_list.sort()
     team_list.insert(0, 'All')
     selected_team = st.selectbox('Team', team_list, index=0, key='team')
